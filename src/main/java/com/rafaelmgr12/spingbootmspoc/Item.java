247f9a0561f6ed83f01b1d08299981cdf363dfbc
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2023-02-12 19:44:44 -0300

    [feat] implement the Item claass

--- src/main/java/com/rafaelmgr12/spingbootmspoc/Item.java
+++ src/main/java/com/rafaelmgr12/spingbootmspoc/Item.java
@@ -6,8 +6,57 @@ import org.springframework.data.annotation.Id;
 
 public class Item {
 
-  //write your code here
-  
+    private @Id String id;
+    private String name;
+    private String description;
+    private double price;
+
+    private Item() {
+    }
+
+    Item(String name, String description, double price) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+    Item(String id, String name, String description, double price) {
+        this(name, description, price);
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) {
