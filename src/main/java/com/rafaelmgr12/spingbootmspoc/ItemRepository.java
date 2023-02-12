5c24d26ff68ead8614afb3df890812b117712254
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2023-02-12 20:00:46 -0300

    [feat] implement the Item repository

--- src/main/java/com/rafaelmgr12/spingbootmspoc/ItemRepository.java
+++ src/main/java/com/rafaelmgr12/spingbootmspoc/ItemRepository.java
@@ -3,4 +3,7 @@ package com.rafaelmgr12.spingbootmspoc;
 import reactor.core.publisher.Mono;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 
-//write your code here
+public interface ItemRepository extends ReactiveCrudRepository<Item, String> {
+
+    Mono<Item> findByName(String name);
+}
