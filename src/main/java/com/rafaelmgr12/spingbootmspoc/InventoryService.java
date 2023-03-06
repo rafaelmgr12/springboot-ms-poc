4d118935174e6ae476b1a8046cc2537a56ec12e8
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2023-03-06 16:46:55 -0300

    [feat] add new Inventory service

--- src/main/java/com/rafaelmgr12/spingbootmspoc/InventoryService.java
+++ src/main/java/com/rafaelmgr12/spingbootmspoc/InventoryService.java
@@ -9,7 +9,67 @@ import org.springframework.stereotype.Service;
 
 @Service
 class InventoryService {
- 
- //write code for task 7,8,9 here
+
+    private ItemRepository itemRepository;
+    private CartRepository cartRepository;
+
+    public InventoryService(ItemRepository itemRepository, CartRepository cartRepository) {
+        this.itemRepository = itemRepository;
+        this.cartRepository = cartRepository;
+    }
+
+    public Mono<Cart> getCart(String cartId){
+        return this.cartRepository.findById(cartId);
+    }
+
+    public Flux<Item> getItems(){
+        return this.itemRepository.findAll();
+    }
+
+    Mono<Item> saveItem(Item newItem) {
+        return this.itemRepository.save(newItem);
+    }
+
+    Mono<Void> deleteItem(String id){
+        return  this.itemRepository.deleteById(id);
+    }
+
+    Mono<Cart> addItemCart(String cartId, String itemId){
+        return this.cartRepository.findById(cartId)
+                .defaultIfEmpty(new Cart(cartId))
+                .flatMap(cart -> cart.getCartItems().stream()
+                        .filter(cartItem -> cartItem.getItem().getId().equals(itemId))
+                        .findAny()
+                        .map(cartItem -> {
+                            cartItem.increment();
+                            return Mono.just(cart);
+                        })
+                        .orElseGet(() -> {
+                            return this.itemRepository.findById(itemId)
+                                    .map(item -> new CartItem(item))
+                                    .map(cartItem -> {
+                                        cart.getCartItems().add(cartItem);
+                                        return cart;
+                                    });
+                        }))
+                .flatMap(cart -> this.cartRepository.save(cart));
+    }
+    Mono<Cart> removeOneFromCart(String cartId, String itemId) {
+        return this.cartRepository.findById(cartId)
+                .defaultIfEmpty(new Cart(cartId))
+                .flatMap(cart -> cart.getCartItems().stream()
+                        .filter(cartItem -> cartItem.getItem().getId().equals(itemId))
+                        .findAny()
+                        .map(cartItem -> {
+                            cartItem.decrement();
+                            return Mono.just(cart);
+                        })
+                        .orElse(Mono.empty()))
+                .map(cart -> new Cart(cart.getId(), cart.getCartItems().stream()
+                        .filter(cartItem -> cartItem.getQuantity() > 0)
+                        .collect(Collectors.toList())))
+                .flatMap(cart -> this.cartRepository.save(cart));
+    }
+
 
 }
